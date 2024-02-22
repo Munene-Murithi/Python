12bc386b607d0d60c141f41cc1a703aa440f44bd
Munene-Murithi <sammymunene22@gmail.com>
2024-02-22 16:28:50 +0300

    user input

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = input("Enter your name:  ")
+age = input("Enter your age:  ")
+location = input("Enter your current location:  ")
+
+print("Hello {}, you are {} years old and you live in {}.".format(name, age, location))
+
